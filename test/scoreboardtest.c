ab2cae30888d2454bbbe2f82fda6f2bcfba7ce97
Hugo Sandell <hugossandell@gmail.com>
2018-03-01 00:15:01 +0100

    Move scoreboardtest.c to test folder, Implement first test partially

---
+++ test/scoreboardtest.c
@@ -0,0 +1,28 @@
+#include "scoreboard.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool test1(){
+  return true;
+}
+
+bool test2(){
+  return true;
+}
+
+bool test3(){
+  return true;
+}
+
+bool test4(){
+  return true;
+}
+
+int main () {
+  test1();
+  test2();
+  test3();
+  test4();
+  return 0;
+}
