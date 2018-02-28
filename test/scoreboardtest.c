119ac6be23024723645fc85b43e5aea2edb65b65
Hugo Sandell <hugossandell@gmail.com>
2018-03-01 00:15:45 +0100

    Implement some of first scoreboard test

--- test/scoreboardtest.c
+++ test/scoreboardtest.c
@@ -3,7 +3,23 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-bool test1(){
+bool test1() {
+  init_scoreboard();
+  if(get_highscores()[0].name != "DBRO") return false;
+  if(get_highscores()[0].score != 9999999999) return false;
+  if(get_highscores()[1].name != "OS K") return false;
+  if(get_highscores()[1].score != 1234500) return false;
+  if(get_highscores()[2].name != "HU S") return false;
+  if(get_highscores()[2].score != 1234500) return false;
+  if(get_highscores()[3].name != "TEST") return false;
+  if(get_highscores()[3].score != 1000000) return false;
+  // ...
+  if(get_highscores()[7].name != "AAAA") return false;
+  if(get_highscores()[7].score != 300000) return false;
+  if(get_highscores()[8].name != "BBBB") return false;
+  if(get_highscores()[8].score != 200000) return false;
+  if(get_highscores()[9].name != "CCCC") return false;
+  if(get_highscores()[9].score != 100000) return false;
   return true;
 }
 
@@ -19,10 +35,15 @@ bool test4(){
   return true;
 }
 
+bool test6(){
+  return true;
+}
+
 int main () {
-  test1();
-  test2();
-  test3();
-  test4();
+  if(!test1()) return 1;
+  if(!test2()) return 2;
+  if(!test3()) return 3;
+  if(!test4()) return 4;
+  if(!test5()) return 5;
   return 0;
 }
