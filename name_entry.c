36b15fefccc3594201f33fc2ee9e07bcd586ed1d
Hugo Sandell <hugossandell@gmail.com>
2018-02-28 14:14:22 +0100

    make progress on name entry

--- name_entry.c
+++ name_entry.c
@@ -10,10 +10,49 @@
 #include "chipkitio.h"
 #include "graphics.h"
 
+int selection;
+char name[4] = {'A', 'A', 'A', 'A'};
+char printbuffer[16];
+
 void draw_ne() {
-  display_string(0, "Enter Your Name")
+  display_string(0, "Enter Your Name");
+
+  strcpy(printbuffer, "                ");
+  if(selection==5)
+    strcpy(printbuffer+11, "^^^^");
+  else
+    printbuffer[selection*2-1] = '^';
+  display_string(1, printbuffer);
+
+  strcpy(printbuffer, "A A A A   [DONE]");
+  printbuffer[0] = name[0];
+  printbuffer[2] = name[1];
+  printbuffer[4] = name[2];
+  printbuffer[6] = name[3];
+  display_string(2, printbuffer);
+
+  strcpy(printbuffer, "                ");
+  if(selection==5)
+    strcpy(printbuffer+11, "vvvv");
+  else
+    printbuffer[selection*2-1] = 'v';
+  display_string(3, printbuffer);
+
   display_update_debug();
 }
-void tick_ne(){
+
+void tick_ne() {
+  selection -= get_button(BTN4);
+  selection += get_button(BTN3);
+  if(selection < 0) selection += 5;
+  if(selection > 4) selection -= 5;
+
+  if(!(get_button(BTN4) || get_button(BTN3))) {
+    name[selection] -= get_button(BTN2);
+    name[selection] += get_button(BTN1);
+    if(name[selection] < 'A') name[selection] += 26;
+    if(name[selection] > 'Z') name[selection] -= 26;
+  }
+  while(get_buttons()){}; //wait until all buttons are released
   draw_ne();
 }
